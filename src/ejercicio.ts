b76ec2d44155f975b2edac11c6b611fe10e3feea
francoracciatti <104107376+francoracciatti@users.noreply.github.com>
2022-04-25 16:15:42 -0300

    Initial commit

---
+++ src/ejercicio.ts
@@ -0,0 +1,11 @@
+console.log("dirigirse al supermecado");
+
+console.log("levantarme de la silla");
+console.log("dirigirme hacia la puerta");
+console.log("abrir la puerta y salir");
+console.log("cerrar la puerta e ir hacia el auto");
+console.log("abrir el auto, subirme y arrancar");
+console.log("ir al supermecado, bajarme del auto y cerrar");
+console.log("abrir la puerta del supermecado y comprar");
+console.log("pagar y salir del supermecado");
+console.log("subirme al auto y volver a mi casa");
